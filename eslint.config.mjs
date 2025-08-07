dde7dbb255c5e5cc9de6d64751641d2ab5bfae1e
Berliana Mahadewi <berlianamahadewi@student.telkomuniversity.ac.id>
2025-08-07 16:50:26 +0700

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+   {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // 🔥 Nonaktifkan rule error
+    },
 ];
 
 export default eslintConfig;
